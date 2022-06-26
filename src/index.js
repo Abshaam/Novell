a08f84cf659935103ce81be02ec62dbd1581c621
Abdul Wahab Ushama <ushamaabdulwahab@gmail.com>
2022-06-26 20:47:46 +0000

    Developed the user interface for adding novel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from './store';
+import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+export const store = configureStore();
+
+
+// subscribe the getState dispatch
+store.subscribe(() => console.log(store.getState()));
+
+
+axios.defaults.headers.post['Content-Type'] = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
